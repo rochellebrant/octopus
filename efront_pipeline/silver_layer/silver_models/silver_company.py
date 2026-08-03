7e19ee97c76d48b0a1bdadfbc22a31247c42cb9e
rochellebrant <144122520+rochellebrant@users.noreply.github.com>
2026-08-03 13:18:54 +0100

    Delete efront_pipeline directory

--- efront_pipeline/silver_layer/silver_models/silver_company.py
+++
@@ -1,6 +0,0 @@
-silver_company_sql_code = """
-SELECT *, current_timestamp() AS refresh_timestamp FROM {bronze_prefix}company ef_c
-LEFT JOIN oegen_data_prod_prod.core_data_model.bronze_mapping_company map_c
-    ON ef_c.Company_IQId = map_c.source_company_id
-    WHERE map_c.source_system_id = 'SRCE_SYST_1001'
-"""
